5dfb087d0731a76e8d5f7a3130c0cf7190d95f6e
Boris <emplonics@gmail.com>
2023-08-20 02:13:04 +0300

    scene orientation controls

--- src/components/BuildingView.vue
+++ src/components/BuildingView.vue
@@ -1,6 +1,6 @@
 <template>
   <div @resize="setSceneSize">
-    <canvas ref="canvasElement" />
+    <canvas ref="canvasElement" tabindex="0" />
   </div>
 </template>
 <script setup lang="ts">
@@ -13,6 +13,8 @@ import { renderBuildingInterior, deserializeBuilding } from '@/services/renderBu
 import { computed, onMounted, ref, watch } from 'vue'
 import { WebGLRenderer } from 'three'
 import type { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import type { PointerLockControls } from 'three/addons/controls/PointerLockControls.js'
+
 import { useBuildingsStore } from '@/stores/buildings'
 
 const buildingInterior = computed(() => useBuildingsStore().buildingInterior)
@@ -22,9 +24,15 @@ const theme = THEMES.default
 
 const MESH_NAME = 'interior'
 
+const mousePosition = ref<THREE.Vector2>(new THREE.Vector2())
+let controls: OrbitControls
+let arrowControls: PointerLockControls
+
 scene.background = new THREE.Color(theme.sky)
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 100)
+const raycaster = new THREE.Raycaster()
 camera.position.set(0, 1.5, 6.5)
+scene.add(camera)
 const canvasElement = ref<HTMLCanvasElement | null>(null)
 
 const { ambientLight, mainLight } = createLights()
@@ -33,22 +41,62 @@ scene.add(ambientLight, mainLight)
 const { ground, grid } = renderGround(theme)
 scene.add(ground, ...grid)
 
-function loop(renderer: WebGLRenderer, controls: OrbitControls) {
+function loop(renderer: WebGLRenderer) {
   requestAnimationFrame(() => {
-    loop(renderer, controls)
+    loop(renderer)
   })
-  controls.update()
+  if (controls && !arrowControls?.isLocked) {
+    controls.update()
+  }
+
   renderer.render(scene, camera)
 }
 
 function setSceneSize() {
-  ;(renderer.value as WebGLRenderer).setSize(window.innerWidth / 2, window.innerHeight / 2)
+  const { width, height } = (canvasElement.value as HTMLCanvasElement).getBoundingClientRect()
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+  ;(renderer.value as WebGLRenderer).setSize(width, height)
 }
+
+function mouseMoveHandler(event: MouseEvent) {
+  const canvas = canvasElement.value as HTMLCanvasElement
+  const { left, top, width, height } = canvas.getBoundingClientRect()
+
+  mousePosition.value.x = ((event.clientX - left) / width) * 2 - 1
+  mousePosition.value.y = -((event.clientY - top) / height) * 2 + 1
+  raycaster.setFromCamera(mousePosition.value, camera)
+
+  const intersects = raycaster.intersectObjects(scene.children, false)
+  const interiorIntersection = intersects.find(
+    (intersection) => intersection.object.name === MESH_NAME
+  )
+  if (!interiorIntersection) {
+    return
+  }
+  const pointerControls = arrowControls as PointerLockControls
+  if (!pointerControls.isLocked) {
+    pointerControls.lock()
+    camera.position.copy(interiorIntersection.point)
+  }
+}
+
 onMounted(() => {
-  renderer.value = new THREE.WebGLRenderer({ canvas: canvasElement.value as HTMLCanvasElement })
+  const canvas = canvasElement.value as HTMLCanvasElement
+  renderer.value = new THREE.WebGLRenderer({ canvas })
   setSceneSize()
-  const controls = createControls(camera, renderer.value.domElement)
-  loop(renderer.value, controls)
+
+  const { controls: threeOrbitControls, arrowControls: threeArrowControls } = createControls(
+    camera,
+    renderer.value.domElement,
+    { avoidCollision: [MESH_NAME] }
+  )
+  controls = threeOrbitControls
+  arrowControls = threeArrowControls
+
+  loop(renderer.value)
+
+  canvas.addEventListener('dblclick', mouseMoveHandler)
 })
 
 watch(buildingInterior, (interior) => {
@@ -61,3 +109,9 @@ watch(buildingInterior, (interior) => {
   }
 })
 </script>
+<style scoped lang="scss">
+canvas {
+  width: 50vw;
+  height: 50vh;
+}
+</style>
--- src/services/controls.ts
+++ src/services/controls.ts
@@ -1,13 +1,105 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js'
 import * as THREE from 'three'
+import { onKeyStroke } from '@vueuse/core'
 
-function createControls(camera: THREE.PerspectiveCamera, canvas: HTMLCanvasElement) {
+type Direction = 'forward' | 'backward' | 'right' | 'left'
+
+function createControls(
+  camera: THREE.PerspectiveCamera,
+  canvas: HTMLCanvasElement,
+  options: { avoidCollision: string[] }
+) {
+  const delta = 0.1
+
   const controls = new OrbitControls(camera, canvas)
   controls.enableDamping = true
   controls.maxPolarAngle = Math.PI / 2.1
   controls.minDistance = 2
   controls.maxDistance = 50
-  return controls
+
+  const arrowControls = new PointerLockControls(camera, canvas)
+
+  const raycaster = new THREE.Raycaster(
+    camera.position.clone(),
+    new THREE.Vector3(0, -1, 0),
+    0,
+    delta
+  )
+
+  let cameraLastPosition = camera.position
+
+  arrowControls.addEventListener('lock', () => {
+    cameraLastPosition = camera.position
+    controls.saveState()
+    controls.enabled = false
+  })
+  arrowControls.addEventListener('unlock', () => {
+    camera.position.copy(cameraLastPosition)
+    controls.reset()
+    controls.enabled = true
+  })
+
+  const arrowOptions = { target: canvas }
+  const directionKeys: Record<Direction, Array<KeyboardEvent['code']>> = {
+    forward: ['w', 'W', 'ArrowUp'],
+    backward: ['s', 'S', 'ArrowDown'],
+    right: ['d', 'D', 'ArrowRight'],
+    left: ['a', 'A', 'ArrowLeft']
+  }
+
+  onKeyStroke(
+    [
+      ...directionKeys.forward,
+      ...directionKeys.backward,
+      ...directionKeys.left,
+      ...directionKeys.right
+    ],
+    (event) => {
+      const direction = Object.keys(directionKeys).find((dir) =>
+        directionKeys[dir as Direction].includes(event.code)
+      ) as Direction
+      raycaster.ray.origin.copy(camera.position.clone())
+      camera.getWorldDirection(raycaster.ray.direction)
+      if (direction === 'backward') {
+        raycaster.ray.direction.setLength(-1)
+        // we can instead use:
+        // raycaster.ray.direction.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI)
+      }
+      if (direction === 'right') {
+        raycaster.ray.direction.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2)
+      }
+      if (direction === 'left') {
+        raycaster.ray.direction.applyAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI * (3 / 2))
+      }
+      raycaster.ray.origin.add(raycaster.ray.direction.setLength(camera.near))
+      if (
+        raycaster
+          .intersectObjects(camera.parent?.children ?? [])
+          .find((int) => options.avoidCollision.includes(int.object.name))
+      ) {
+        return
+      }
+      switch (direction) {
+        case 'forward':
+          arrowControls.moveForward(delta)
+          break
+        case 'backward':
+          arrowControls.moveForward(-delta)
+          break
+        case 'right':
+          arrowControls.moveRight(delta)
+          break
+        case 'left':
+          arrowControls.moveRight(-delta)
+          break
+        //TODO: collision check while collateral moving
+      }
+    },
+    arrowOptions
+  )
+
+  return { controls, arrowControls }
 }
 
 export { createControls }
